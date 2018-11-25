32b136fab87213be4712ccc505e1cf9ab9bee4b6
Arley Lambert <32528396+arleylambert@users.noreply.github.com>
2018-11-25 20:40:33 -0200

    RC 0.9

--- scripts/pihole_conf.sh
+++ scripts/pihole_conf.sh
@@ -6,7 +6,7 @@ RUN_UUID=$(cat /proc/sys/kernel/random/uuid)
 
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-STEPOK="[\e[32m ✔ \e[0m]"
+STEPOK="[\e[32m ✓ \e[0m]"
 STEPNO="[\e[31m X \e[0m]"
 STEPDOT="[\e[1m...\e[0m]"
 
@@ -44,7 +44,7 @@ fi
 
 #==========| Whitelist Domains |==========
 
-curl -sS https://raw.githubusercontent.com/arlambert/piholfig/master/scripts/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt # >/dev/null
+curl -sS https://raw.githubusercontent.com/arlambert/piholfig/master/scripts/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
 echo -e " ${STEPDOT} \e[1m Adding to whitelist... \e[0m"
 sleep 0.5
 
@@ -57,7 +57,7 @@ echo -e " ${STEPOK} \e[32m Whitelist Updated! \e[0m"
 #==========| ADLists |==========
 
 # Get the updated adlists.list and update pihole
-curl -sS "https://v.firebog.net/hosts/lists.php?type=nocross" | sudo tee -a /etc/pihole/adlist.list # >/dev/null
+curl -sS "https://v.firebog.net/hosts/lists.php?type=nocross" | sudo tee -a /etc/pihole/adlist.list >/dev/null
 echo -e " ${STEPDOT} \e[1m Updating ADList... \e[0m"
 sleep 0.5
 
@@ -70,7 +70,7 @@ echo -e " ${STEPOK} \e[32m ADlist Updated! \e[0m"
 #==========| Updating Gravity |==========
 
 echo -e " ${STEPDOT} \e[1m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-pihole -g #>/dev/null
+pihole -g >/dev/null
 wait
 echo -e " ${STEPOK} \e[32m Pi-hole's gravity updated! \e[0m"
 
