2f288e7e11ff379a62315ae6aa2420d538ceb5c5
Arley Lambert <32528396+arleylambert@users.noreply.github.com>
2018-11-25 20:38:02 -0200

    New Error line 73

--- scripts/pihole_conf.sh
+++ scripts/pihole_conf.sh
@@ -70,7 +70,7 @@ echo -e " ${STEPOK} \e[32m ADlist Updated! \e[0m"
 #==========| Updating Gravity |==========
 
 echo -e " ${STEPDOT} \e[1m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-pihole -g > #/dev/null
+pihole -g #>/dev/null
 wait
 echo -e " ${STEPOK} \e[32m Pi-hole's gravity updated! \e[0m"
 
